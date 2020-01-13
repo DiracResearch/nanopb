cc64fbd493047044b7551d242e395ae6ddc6cf98
Bo Langgaard Lind <bo.lind@dirac.com>
2020-01-13 16:03:55 +0100

    Added 'f' suffix to floats
    This is a port of (some of) the fixes for issue #453 in the real nanopb:
    https://github.com/nanopb/nanopb/issues/453

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -405,6 +405,12 @@ class Field:
                 inner_init = str(self.default) + 'ull'
             elif self.pbtype in ['SFIXED64', 'INT64']:
                 inner_init = str(self.default) + 'll'
+            elif self.pbtype == 'FLOAT':
+                inner_init = str(self.default)
+                if not '.' in inner_init:
+                    inner_init += '.0f'
+                else:
+                    inner_init += 'f'
             else:
                 inner_init = str(self.default)
 
